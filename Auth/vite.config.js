3fc15ded4b1544a444921c0defd7b44d72695f27
Muneeb Ur Rehman <muneebsurrehman@gmail.com>
2025-06-15 21:23:38 +0500

    used ngrok sttatic paths

--- Auth/vite.config.js
+++ Auth/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allows external access
+    allowedHosts: [
+      'localhost',
+      '.ngrok-free.app' // Allows all ngrok-free.app subdomains
+    ]
+  }
 })
